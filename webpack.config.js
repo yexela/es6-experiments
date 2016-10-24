6c1a8b7dd1f4f0fcd1e0578586c156911f44df40
Alexey Chernetskiy <alexey.chernetskiy@thomascookonline.com>
2016-10-24 20:28:57 +0300

    - wip.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 'use strict';
-
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const webpack = require('webpack');
+require('babel-polyfill');
 
 module.exports = {
     context: __dirname + '/frontend',
     entry: {
         counter: './redux/counter/counter',
-        todos: './redux/todos/index'
+        todos: ['babel-polyfill', './redux/todos/index']
     },
     output: {
         path: __dirname + '/public',
